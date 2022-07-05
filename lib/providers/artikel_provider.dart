f058aa7a2497e6a11d7b5043036f45bf3ce1de02
rizkirone <102182778+rizkirone@users.noreply.github.com>
2022-07-05 16:31:47 +0700

    Add files via upload

--- lib/providers/artikel_provider.dart
+++ lib/providers/artikel_provider.dart
@@ -1,4 +1,5 @@
 import 'package:artikel/models/artikel_model.dart';
+import 'package:artikel/models/detail_artikel_model.dart';
 
 import 'package:artikel/services/artikel_services.dart';
 import 'package:flutter/cupertino.dart';
@@ -10,15 +11,17 @@ class ArtikelProvider extends ChangeNotifier{
 
   final ArtikelService _artikelService =ArtikelService();
   artikel Artikel =artikel();
+  detail_artikel Detail_artikel= detail_artikel();
 
 
   showListArtikel() async {
     Artikel = await _artikelService .tampilkanartikel();
-
+    notifyListeners();
 
   }
+
 ShowDetailArtikel({String? inputKey})async{
-  Artikel =await _artikelService.tampilkandetail(key: inputKey);
+  Detail_artikel = await _artikelService.tampilkanDetail_artikel(id: inputKey);
     notifyListeners();
 }
 
