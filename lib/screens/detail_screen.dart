d86aa7841248df5d490ce8eceda1d64f53f49748
rizkirone <102182778+rizkirone@users.noreply.github.com>
2022-07-05 16:29:54 +0700

    Add files via upload

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:artikel/providers/artikel_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DetailArtikelScreen extends StatefulWidget{
   final String? title;
@@ -16,9 +17,43 @@ class _DetailArtikelScreenState extends State<DetailArtikelScreen>{
     return Scaffold(
       appBar: AppBar(
         title : Text("${widget.title}"),
-
+        backgroundColor: Colors.cyan,
+        centerTitle: true,
       ),
       
+
+            body:
+      Consumer<ArtikelProvider>(
+        // ignore: non_constant_identifier_names
+        builder: (context, provider, _) {
+          return ListView.builder(
+            itemCount: provider.Detail_artikel.data?.length?? 0,  
+            itemBuilder:(context, index) {
+              return ListTile(
+                title: Column(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.height,
+                      child: Image.network(
+                        provider.Detail_artikel.data![index].image!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text(
+                   provider.Detail_artikel.data![index].title!, 
+                   style: const TextStyle(
+                    fontSize: 24,
+                   ), 
+                    ),
+                    Text(provider.Detail_artikel.data![index].createdAt!),
+                    Text(provider.Detail_artikel.data![index].description!),
+                  ],
+                ),
+              );
+            }
+          );
+        },
+      ),
     );
   }
 }
